2e1de10f65f19a37c6b478df88bb540569175886
Ihimjekwe-Stanley <ihimjekwestanleyc@gmail.com>
2026-07-08 10:16:36 +0100

    made corrrection on the name arrangement

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -7,7 +7,7 @@ class Program
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
 
         Console.Write("What is your frist name?");
-        String name1 = Console.ReadLine();
+        String name1= Console.ReadLine();
         Console.Write("What is your last name?");
         String name2 = Console.ReadLine();
         Console.Write($"Your name is {name2}, {name1} {name2}.");
